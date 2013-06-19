c3f390f661959253b7fce7c484df7137bc67cd01
Rowan Crawford <wombleton@gmail.com>
2013-06-19 19:25:59 +1200

    cache data in memory between parses

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,28 @@
 var _ = require('underscore'),
-    db = require('../lib/db');
+    cache = require('../lib/cache'),
+    url = require('url');
 
 /*
  * GET home page.
  */
 
 exports.data = function(req, res) {
-    var year = req.query.year,
-        startkey = [],
-        endkey = [{}];
+    var year = req.query.year;
 
-    if (year) {
-        startkey.unshift(Number(year));
-        endkey.unshift(Number(year));
-    }
-
-    db.view('crawler/list', {
-        include_docs: true,
-        startkey: startkey,
-        endkey: endkey
-    }, function(err, results) {
-        var stats = _.reduce(results, function(memo, result) {
-            var doc = result.doc,
-                events = doc.info_request_events,
-                title = result.key[1],
-                status = doc.described_state,
-                series,
-                body;
-
-            body = memo[title] = memo[title] || {
-                title: title,
-                waiting: 0,
-                successful: 0,
-                unsuccessful: 0,
-                not_held: 0,
-                total: 0
-            };
-
-            if (_.contains('waiting_response waiting_clarification internal_review'.split(' '), status)) {
-                body.waiting++;
-                body.total++;
-            } else if (_.contains('rejected'.split(' '), status)) {
-                body.unsuccessful++;
-                body.total++;
-            } else if (_.contains('partially_successful successful'.split(' '), status)) {
-                body.successful++;
-                body.total++;
-            } else if (_.contains('not_held'.split(' '), status)) {
-                body.not_held++;
-                body.total++;
-            } else if (_.contains('user_withdrawn'.split(' '), status)) {
-                // do nothing
-            } else {
-                // do nothing
-            }
-
-            return memo;
-        }, {});
-
-        res.send(stats);
+    cache.forYear(year || 'all', function(err, stats) {
+        if (err) {
+            res.send(500, 'Database problem.');
+        } else {
+            res.send(stats);
+        }
     });
 }
 
 exports.index = function(req, res) {
-    db.view('crawler/list', {
-        descending: true,
-        startkey: [{}],
-        endkey: []
-    }, function(err, results) {
-        var years = _.reduce(results, function(memo, result) {
-            var year = result.key[0];
-
-            if (!~memo.indexOf(year)) {
-                memo.push(year);
-            }
-            return memo;
-        }, []);
+    cache.getYears(function(err, years) {
         res.render('index', {
+            law: process.env.ALAVETELI_LAW || "OIA",
+            host: url.parse(process.env.ALAVETELI).host,
             years: years
         });
     });
