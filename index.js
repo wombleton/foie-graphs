d12554a6ebc1e89d33700279df2d43e66ee26355
Rowan Crawford <wombleton@gmail.com>
2013-10-09 12:21:26 +1300

    Add pager to output github pages, use node-dirty

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var Search = require('./lib/search'),
     Scraper = require('./lib/scraper'),
-    Store = require('./lib/store');
+    Store = require('./lib/store'),
+    search,
+    scraper,
+    store;
 
 search = new Search(process.env.ALAVETELI);
 scraper = new Scraper(process.env.ALAVETELI);
-store = new Store(process.env.USER, process.env.REPO, process.env.TOKEN);
+store = new Store();
 
 search.on('request', function(uri) {
     scraper.push(uri);
@@ -18,13 +21,13 @@ scraper.on('drain', function() {
     var now = moment(),
         restart = moment().add(7, days);
 
-    store.flush();
+    new Pager(process.env.USER, process.env.REPO, process.env.TOKEN).write(store);
 
     _.delay(crawl, restart.valueOf() - now.valueOf());
 });
 
 function crawl() {
-    search.start(30);
+    search.start(12);
 }
 
 crawl();
